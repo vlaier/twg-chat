65b89454836ad62b8577e435fa385449f9d8e04c
vlaier <piotrzielu.1996@gmail.com>
2023-11-08 23:57:10 +0100

    feat: add ApolloProvider and configuration

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { API_TOKEN } from '@env';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Rooms } from './src/screens/Rooms';
+import { setContext } from '@apollo/client/link/context';
+
 export default function App() {
+  const httpLink = createHttpLink({
+    uri: 'https://chat.thewidlarzgroup.com/api/graphql',
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: API_TOKEN ? `Bearer ${API_TOKEN}` : '',
+      },
+    };
+  });
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Rooms />
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
 
