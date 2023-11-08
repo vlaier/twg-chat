1c6cde024a5b73aff4befe01ecc9e59d739970a2
vlaier <piotrzielu.1996@gmail.com>
2023-11-08 12:39:57 +0100

    feat: add eslint and prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  root: true,
+  extends: ['universe/native'],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  bracketSpacing: true,
+  printWidth: 80,
+  tabWidth: 2,
+  bracketSameLine: true,
+  useTabs: false,
+  arrowParens: 'avoid',
+};
