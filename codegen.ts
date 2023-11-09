8aae20892d99db0b41b35d70f3cd3ff07d3c4059
vlaier <piotrzielu.1996@gmail.com>
2023-11-09 16:39:38 +0100

    build: add codegen for graphql types generation

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'https://chat.thewidlarzgroup.com/api/graphiql',
+  documents: 'src/graphql/**/*.ts',
+  generates: {
+    'src/gql/': {
+      preset: 'client',
+      plugins: [],
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+};
+
+export default config;
