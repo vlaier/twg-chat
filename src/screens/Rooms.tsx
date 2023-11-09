9c91a75f85f40f1a971a7e9fdc49c33e35c78768
vlaier <piotrzielu.1996@gmail.com>
2023-11-09 16:54:04 +0100

    feat: display rooms and cleanup

--- src/screens/Rooms.tsx
+++ src/screens/Rooms.tsx
@@ -1,25 +1,28 @@
 import { Text, StyleSheet, View } from 'react-native';
 
 import { Room } from '../components/Room';
-import { useRooms } from '../hooks/useRooms';
+import { useUsersRooms } from '../hooks/useUsersRooms';
+import { colors } from '../styles/colors';
+
 export const Rooms = () => {
-  const { data, loading } = useRooms();
+  const { data, loading } = useUsersRooms();
   if (loading) return <Text>Loading...</Text>;
-  const ids = data?.usersRooms.rooms.map(room => room.id);
-  console.log('IDS', ids);
+  const ids = data?.usersRooms?.rooms?.map(room => room?.id);
 
   return (
     <View style={styles.container}>
-      <Text>Rooms</Text>
-      <Room />
-      <View />
+      {ids?.map(id => {
+        if (typeof id !== 'string') return null;
+        return <Room id={id} />;
+      })}
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: colors.veryLightBlue,
     flex: 1,
+    gap: 8,
     height: '100%',
     width: '100%',
   },
