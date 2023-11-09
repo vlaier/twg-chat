732a729868a15b2249668400e5cce175bd575f06
vlaier <piotrzielu.1996@gmail.com>
2023-11-09 01:40:58 +0100

    feat: create separate colors file

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  plum: '#5603AD',
+  darkPlum: '#390273',
+  lightPlum: '#993AFC',
+  veryLightPlum: '#C692FD',
+  blue: '#259DFA',
+  lightBlue: '#B6DEFD',
+  veryLightBlue: '#F0F8FF',
+  gray: '#9FA2B2',
+  lightGray: '#BFC1CC',
+  veryLightGray: '#D9DAE0',
+  white: '#FFFFFF',
+  black: '#1A1A1A',
+  active: '#A8FF76',
+  error: '#FF445A',
+};
