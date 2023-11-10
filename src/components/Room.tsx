d2e905f09708b5e974b731b03cedf2753bf3b531
vlaier <piotrzielu.1996@gmail.com>
2023-11-10 17:51:49 +0100

    feat: add how long ago was last message

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,11 +1,18 @@
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
-import { StyleSheet, Text, TouchableOpacity } from 'react-native'
-
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import Profile from '../../assets/profile.svg'
 import { useMessagesSubscription } from '../hooks/useMessagesSubscription'
 import { useRoom } from '../hooks/useRoom'
 import { RootStackParamList, Routes } from '../navigators/types'
 import { colors } from '../styles/colors'
+import {
+  format,
+  formatDistance,
+  formatRelative,
+  subDays,
+  subMinutes,
+} from 'date-fns'
 
 type RoomProps = {
   id: string
@@ -21,12 +28,43 @@ export const Room = (props: RoomProps) => {
         roomId: data?.room?.id,
       })
   }
-  const lastMessage =
-    data?.room?.messages && data.room.messages[data.room.messages.length - 1]
+  const lastMessage = data?.room?.messages && data.room.messages[0]
+
+  const timeSinceLastMessage = lastMessage?.insertedAt
+    ? formatDistance(
+        subMinutes(new Date(lastMessage?.insertedAt), 0),
+        new Date(),
+      )
+    : ''
 
   return (
     <TouchableOpacity style={styles.room} onPress={handlePress}>
-      {loading ? <Text>Loading...</Text> : <Text>{lastMessage?.body}</Text>}
+      <View style={styles.internalContainer}>
+        {loading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <>
+            <Profile />
+            <View style={styles.textContainer}>
+              <Text
+                style={styles.roomHeader}
+                numberOfLines={1}
+                ellipsizeMode="tail">
+                {data?.room?.name}
+              </Text>
+              <Text
+                style={styles.roomText}
+                numberOfLines={1}
+                ellipsizeMode="tail">
+                {lastMessage?.body}
+              </Text>
+            </View>
+            <View style={styles.timeContainer}>
+              <Text style={styles.time}>{timeSinceLastMessage}</Text>
+            </View>
+          </>
+        )}
+      </View>
     </TouchableOpacity>
   )
 }
@@ -34,7 +72,37 @@ const styles = StyleSheet.create({
   room: {
     width: '100%',
     height: 88,
-    backgroundColor: colors.plum,
+    backgroundColor: colors.white,
     borderRadius: 12,
+    paddingVertical: 12,
+    paddingLeft: 16,
+    paddingRight: 37,
+  },
+  internalContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+  },
+  textContainer: {
+    width: 160,
+  },
+  roomHeader: {
+    fontSize: 15,
+    fontWeight: '500',
+    lineHeight: 20,
+  },
+  roomText: {
+    fontSize: 14,
+    fontWeight: '400',
+  },
+  timeContainer: {
+    alignSelf: 'flex-start',
+  },
+  time: {
+    fontSize: 12,
+    fontWeight: '400',
+    lineHeight: 16,
+    color: colors.lightGray,
   },
 })
