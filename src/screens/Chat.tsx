315bf390469ed32ac41e1c984658f5d7e5cec811
vlaier <piotrzielu.1996@gmail.com>
2023-11-10 18:24:14 +0100

    style: add styling and create render functions for chat components

--- src/screens/Chat.tsx
+++ src/screens/Chat.tsx
@@ -1,15 +1,30 @@
 import { StackScreenProps } from '@react-navigation/stack'
+import debounce from 'lodash.debounce'
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import debounce from 'lodash.debounce'
-import { GiftedChat, IMessage } from 'react-native-gifted-chat'
+import {
+  Bubble,
+  BubbleProps,
+  Composer,
+  ComposerProps,
+  GiftedChat,
+  IMessage,
+  InputToolbar,
+  InputToolbarProps,
+  Send,
+  SendProps,
+} from 'react-native-gifted-chat'
+import TypingIndicator from 'react-native-gifted-chat/lib/TypingIndicator'
 
+import ProfileIcon from '../../assets/profile.svg'
+import SendIcon from '../../assets/send.svg'
 import { CoreMessageFieldsFragment } from '../gql/graphql'
 import { useRoom } from '../hooks/useRoom'
 import { useSendMessage } from '../hooks/useSendMessage'
-import { RootStackParamList, Routes } from '../navigators/types'
-import { useTypingUser } from '../hooks/useTypingUser'
 import { useTypingSubscription } from '../hooks/useTypingSubscription'
+import { useTypingUser } from '../hooks/useTypingUser'
+import { RootStackParamList, Routes } from '../navigators/types'
+import { colors } from '../styles/colors'
 
 const convertToGiftedMessages = (messages: CoreMessageFieldsFragment[]) => {
   const giftedMessages: IMessage[] = messages.map(message => {
@@ -77,21 +92,77 @@ export const Chat = (
     () => debounce(handleTextChange, 300),
     [],
   )
+  const renderInputToolbar = (props: InputToolbarProps<IMessage>) => {
+    return (
+      <InputToolbar
+        {...props}
+        containerStyle={styles.chatInputContainer}
+        renderComposer={renderComposer}
+        renderSend={renderSend}
+      />
+    )
+  }
+  const renderComposer = (props: ComposerProps) => {
+    return (
+      <Composer {...props} textInputStyle={styles.chatInput} placeholder="" />
+    )
+  }
+  const renderSend = (props: SendProps<IMessage>) => {
+    return (
+      <Send {...props} containerStyle={styles.sendButton} alwaysShowSend>
+        <SendIcon />
+      </Send>
+    )
+  }
+  const renderBubble = (props: BubbleProps<IMessage>) => {
+    return (
+      <Bubble
+        {...props}
+        renderTime={() => null}
+        containerStyle={{
+          left: bubbleStyles.left.container,
+          right: bubbleStyles.left.container,
+        }}
+        wrapperStyle={{
+          left: bubbleStyles.left.wrapper,
+          right: bubbleStyles.right.wrapper,
+        }}
+        textStyle={{
+          left: bubbleStyles.left.text,
+          right: bubbleStyles.right.text,
+        }}
+      />
+    )
+  }
+  const renderFooter = () => {
+    if (isTyping)
+      return (
+        <View style={styles.typingIndicator}>
+          <ProfileIcon height={24} width={24} style={{ marginBottom: 12 }} />
+          <TypingIndicator isTyping={isTyping} />
+        </View>
+      )
+    return null
+  }
 
   if (loading) return <Text>Loading...</Text>
   return (
     <View style={styles.chatContainer}>
-      <Text>CHAT: {data?.room?.name}</Text>
-      <Text>{giftedChatMessages.length}</Text>
       <GiftedChat
+        renderDay={() => null}
+        showAvatarForEveryMessage
+        renderAvatar={() => null}
+        renderInputToolbar={renderInputToolbar}
+        renderBubble={renderBubble}
         messages={giftedChatMessages}
         onSend={messages => handleSend(messages)}
         user={{
           _id: userId || '',
         }}
         keyboardShouldPersistTaps="never"
-        isTyping={isTyping}
         onInputTextChanged={debouncedTextChangeHandler}
+        minInputToolbarHeight={102}
+        renderFooter={renderFooter}
       />
     </View>
   )
@@ -99,5 +170,80 @@ export const Chat = (
 const styles = StyleSheet.create({
   chatContainer: {
     flex: 1,
+    backgroundColor: colors.veryLightBlue,
+  },
+  chatInputContainer: {
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+    height: 102,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: colors.lightBlue,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  chatInput: {
+    borderRadius: 12,
+    borderBottomRightRadius: 0,
+    padding: 12,
+    justifyContent: 'center',
+    backgroundColor: colors.white,
+    flex: 1,
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 20,
+  },
+  sendButton: {
+    width: 44,
+    height: 44,
+    color: colors.white,
+    marginLeft: 12,
+  },
+  typingIndicator: {
+    flexDirection: 'row',
+    marginLeft: 16,
+    marginBottom: 24,
+    alignItems: 'flex-end',
+    gap: 12,
   },
 })
+const bubbleStyles = {
+  left: StyleSheet.create({
+    container: {
+      marginVertical: 6,
+      paddingLeft: 52,
+    },
+    wrapper: {
+      width: 245,
+
+      borderRadius: 12,
+      borderBottomLeftRadius: 0,
+      backgroundColor: colors.white,
+      padding: 12,
+    },
+    text: {
+      color: colors.black,
+      fontSize: 14,
+      fontStyle: 'normal',
+      fontWeight: '400',
+      lineHeight: 20,
+    },
+  }),
+  right: StyleSheet.create({
+    wrapper: {
+      borderRadius: 12,
+      borderBottomRightRadius: 0,
+      backgroundColor: colors.lightPlum,
+      padding: 12,
+    },
+    text: {
+      color: colors.white,
+      fontSize: 14,
+      fontStyle: 'normal',
+      fontWeight: '400',
+      lineHeight: 20,
+    },
+  }),
+}
